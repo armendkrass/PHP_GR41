b9aae27dec893b670976ebc9977018efad7d0c16
agonmustafa1 <58729229+agonmustafa1@users.noreply.github.com>
2020-05-12 16:30:06 +0200

    Create apliko.php

---
+++ phpFk/apliko.php
@@ -0,0 +1,23 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+	<title>
+		Apliko tani!
+	</title>
+	<link rel="stylesheet" type="text/css" href="perlogin.css">
+
+</head>
+<body>	
+
+		<?php
+			if(isset($_POST['create']))
+			{
+				echo 'Keni aplikuar me sukses';
+			}
+		?>
+	
+		<p>Zgjedh Open University Tani!</p>
+		<div class="katrori">
+			<img src="foto/useri.png" class="useri">
+				<h1>Mbushni te dhenat tuaja personale</h1>
+					<form  action="">
