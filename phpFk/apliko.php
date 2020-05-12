88740e076115e35030a61e0773c9d11bd1ecec18
agonmustafa1 <58729229+agonmustafa1@users.noreply.github.com>
2020-05-12 16:31:20 +0200

    Update apliko.php

--- phpFk/apliko.php
+++ phpFk/apliko.php
@@ -21,3 +21,19 @@
 			<img src="foto/useri.png" class="useri">
 				<h1>Mbushni te dhenat tuaja personale</h1>
 					<form  action="">
+						<p>Emri</p>
+							<input type="text" name="emri" required="" placeholder="Emri">
+						<p>Mbiemri</p>
+							<input type="text" name="mbiemri" required=""  placeholder="Mbiemri">
+						<p>Datelindja</p>
+							<input type="date" name="datelindja" required="" placeholder="Datelindje">
+						<p>E-mail</p>
+							<input type="email" name="email" required="" placeholder="E-mail">
+						<p>Vendi i lindjes</p>
+							<input type="text" name="vl" required=""  placeholder="Vendi i lindjes">
+						<p>Adresa e banimit</p>
+							<input type="text" name="banon" required=""  placeholder="Adresa e banimit">
+						<p>Numri i telefonit</p>
+							<input type="number" name="number" required=""  placeholder="+383-04#-###-###">
+						<p>Zgjedhni drejtimin</p>
+							<select class ="droplist" required="">
